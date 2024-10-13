227ba2ff9d098779a4f8ec0a54b517df8cdd122b
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-13 14:09:38 +0530

    Clear git cache and update .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/karthikkk/Developer/GitHub/zentask/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
