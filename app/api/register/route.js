9dddfbdd61b7452028d862825f4ad7646ce2a193
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-14 10:05:40 +0530

    Modified Registeration Backend Code

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,23 +1,44 @@
+import dbConnect from '@/app/lib/dbConnect';
+import User from '@/app/models/User';
+import bcrypt from 'bcryptjs';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
     console.log('Received request:', request);
 
     try {
-        const body = await request.json();
-        console.log('Received body:', body);
-        
-        
+	await dbConnect();
+
+	const body = await request.json();
+	console.log("Request body: ", body);
+
+	const { username, email, password } = body;
+
+	const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+	if (existingUser) {
+		return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+    }
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const newUser = new User({
+		username,
+		email,
+		password: hashedPassword,
+	});
+
+	await newUser.save();
 
         return NextResponse.json(
             { message: 'User registered successfully' },
             { status: 201 }
         );
     } catch (error) {
-        console.error('Error parsing request body:', error);
+        console.error('Registration Error:', error);
         return NextResponse.json(
-            { error: 'Invalid request body' },
-            { status: 400 }
+            { error: 'Registration Failed' },
+            { status: 500 }
         );
     }
 }
